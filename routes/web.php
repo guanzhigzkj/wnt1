f4fa3f5446d6ecb96d16c05d937de243eee75e96
songanqi@baojia.com <songanqi@baojia.com>
2019-01-25 18:43:11 +0800

    0125

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
 Route::get('/', 'UserController@login')->name('login');
